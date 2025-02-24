2f9a144332dfe4e2f15941355b96364269564370
Deminarcis <jake.maher.10@gmail.com>
2025-02-25 00:25:47 +1100

    wrong path used in installer scripts

--- Kernels/6.1-lts/linux-msft-wsl-6.1.21.2/install-kernel.sh
+++ Kernels/6.1-lts/linux-msft-wsl-6.1.21.2/install-kernel.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 if [ -f "/etc/wsl.conf" ]; then
-    powershell.exe /C 'Copy-Item .\arch\x86\boot\bzImage $env:USERPROFILE'
+    powershell.exe /C 'Copy-Item .\bzImage $env:USERPROFILE'
     powershell.exe /C 'Write-Output [wsl2]`nkernel=$env:USERPROFILE\bzImage | % {$_.replace("\","\\")} | Out-File $env:USERPROFILE\.wslconfig -encoding ASCII'
-    echo '[!!] Build done! The kernel has been moved for you and set up'
+    echo '[!!] The kernel has been moved for you and set up'
 else
-    echo -e "[!!] Build done! Please move the bzImage file from 'kernel-wsl2/arch/x86/boot' to your Windows PC or VM and add the kernel to your .wslconfig"
+    echo -e "[!!] Please move the bzImage file to your Windows PC or VM and add the kernel to your .wslconfig"
 fi
